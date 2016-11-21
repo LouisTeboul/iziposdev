99b94d0e7303c7c67cde91fb1720adc40bf9dad3
Bastien <bastien.lamugniere@gmail.com>
2016-11-21 17:40:58 +0100

    First commit

---
+++ www/js/controllers/modals/ModalCustomerController.js
@@ -0,0 +1,27 @@
+﻿app.controller('ModalCustomerController', function ($scope, $rootScope, $uibModalInstance,shoppingCartService) {
+    var current = this; 
+    
+
+
+    $scope.init = function () {
+
+        $scope.searchResults = [];
+    }
+
+    $scope.searchForCustomer = function (query) {
+        shoppingCartService.searchForCustomerAsync(query).then(function (res) {
+            $scope.searchResults = res;
+        }, function () {
+            $scope.searchResults = [];
+        });
+    };
+
+
+    $scope.ok = function () {
+    }
+
+    $scope.close = function () {
+        $uibModalInstance.close();
+    }
+
+});
